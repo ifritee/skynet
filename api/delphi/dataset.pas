09f05181fe4704216e9de844b4d4e8e048e14620
ifritee <ifritee@gmail.com>
2020-04-29 11:06:53 +0300

    Добавил новые демо, добавил документы

--- api/delphi/dataset.pas
+++ api/delphi/dataset.pas
@@ -8,41 +8,89 @@ type
   PPSingle = ^PSingle;
   PPByte = ^PByte;
 
-   Function mnistTrainData(datafilename : PAnsiChar;
-                          labelfilename : PAnsiChar;
-                          data_ : PPSingle;
-                          label_ : PPSingle;
-                          sizeData : PLayerSize;
-                          sizeLabel : PLayerSize;
-                          qty: Integer; step: Integer
-                          ) : keras.TStatus; cdecl; external KERAS_EXPORT;
+/// @brief mnistTrainData Считывание данных из БД MNIST
+/// @param datafilename Имя файла с данными
+/// @param labelfilename Имя файла с метками
+/// @param data Хранилище данных для записи
+/// @param label Хранилище меток для записи
+/// @param sizeData Параметры считываемых данных
+/// @param sizeLabel Параметры считываемых меток
+/// @param qty Количество, считываемое за раз
+/// @param step Шаг считывания
+/// @return Состояние считывания
+Function mnistTrainData(datafilename : PAnsiChar;
+                        labelfilename : PAnsiChar;
+                        data_ : PPSingle;
+                        label_ : PPSingle;
+                        sizeData : PLayerSize;
+                        sizeLabel : PLayerSize;
+                        qty: Integer; step: Integer
+                        ) : keras.TStatus; cdecl; external KERAS_EXPORT;
 
-  Function bikeTrainData(filename : PAnsiChar;
-                         isDay : Boolean;
+/// @brief bikeTrainData Считывание данных о количестве велосипедов взятых в аренду
+/// @param filename Имя файла с данными
+/// @param isDay Флаг (0 - за час, 1 - за день)
+/// @param data Хранилище данных для записи
+/// @param label Хранилище меток для записи
+/// @param sizeData Параметры считываемых данных
+/// @param sizeLabel Параметры считываемых меток
+/// @param qty Количество, считываемое за раз
+/// @param step Шаг считывания
+/// @return Состояние считывания
+Function bikeTrainData(filename : PAnsiChar;
+                       isDay : Boolean;
+                       data_ : PPSingle;
+                       label_ : PPSingle;
+                       sizeData : PLayerSize;
+                       sizeLabel : PLayerSize;
+                       qty: Integer; step: Integer
+                       ) : keras.TStatus; cdecl; external KERAS_EXPORT;
+
+/// @brief bostonTrainData Считывание данных о ценах аренды квартир в Бостоне
+/// @param filename Имя файла с данными
+/// @param data Хранилище данных для записи
+/// @param label Хранилище меток для записи
+/// @param sizeData Параметры считываемых данных
+/// @param sizeLabel Параметры считываемых меток
+/// @param qty Количество, считываемое за раз
+/// @param step Шаг считывания
+/// @return Состояние считывания
+Function bostonTrainData(filename : PAnsiChar;
                          data_ : PPSingle;
                          label_ : PPSingle;
                          sizeData : PLayerSize;
                          sizeLabel : PLayerSize;
-                          qty: Integer; step: Integer
+                         qty: Integer; step: Integer
                          ) : keras.TStatus; cdecl; external KERAS_EXPORT;
 
-  Function bostonTrainData(filename : PAnsiChar;
-                           data_ : PPSingle;
-                           label_ : PPSingle;
-                           sizeData : PLayerSize;
-                           sizeLabel : PLayerSize;
-                          qty: Integer; step: Integer
-                           ) : keras.TStatus; cdecl; external KERAS_EXPORT;
+/// @brief breastTrainData Считывание данных о заболевших раком в Висконсине
+/// @param filename Имя файла с данными
+/// @param flag Флаг, показывающий тип считываемой информации (1 - Wisconsin, 2 - wdbc, 3 - wpbc)
+/// @param data Хранилище данных для записи
+/// @param label Хранилище меток для записи
+/// @param sizeData Параметры считываемых данных
+/// @param sizeLabel Параметры считываемых меток
+/// @param qty Количество, считываемое за раз
+/// @param step Шаг считывания
+/// @return Состояние считывания
+Function breastTrainData(filename : PAnsiChar;
+                         flag_ : Integer;
+                         data_ : PPSingle;
+                         label_ : PPSingle;
+                         sizeData : PLayerSize;
+                         sizeLabel : PLayerSize;
+                         qty: Integer; step: Integer
+                         ) : keras.TStatus; cdecl; external KERAS_EXPORT;
 
-  Function breastTrainData(filename : PAnsiChar;
-                           flag_ : Integer;
-                           data_ : PPSingle;
-                           label_ : PPSingle;
-                           sizeData : PLayerSize;
-                           sizeLabel : PLayerSize;
-                          qty: Integer; step: Integer
-                           ) : keras.TStatus; cdecl; external KERAS_EXPORT;
-
+/// @brief irisTrainData Считывание данных о сортах Ирисов (Ирисы Фишера)
+/// @param filename Имя файла с данными
+/// @param data Хранилище данных для записи
+/// @param label Хранилище меток для записи
+/// @param sizeData Параметры считываемых данных
+/// @param sizeLabel Параметры считываемых меток
+/// @param qty Количество, считываемое за раз
+/// @param step Шаг считывания
+/// @return Состояние считывания
 Function irisTrainData(filename : PAnsiChar;
                            data_ : PPSingle;
                            label_ : PPByte;
@@ -51,6 +99,15 @@ Function irisTrainData(filename : PAnsiChar;
                           qty: Integer; step: Integer
                            ) : keras.TStatus; cdecl; external KERAS_EXPORT;
 
+/// @brief wineTrainData Считывание данных о сортах вин
+/// @param filename Имя файла с данными
+/// @param data Хранилище данных для записи
+/// @param label Хранилище меток для записи
+/// @param sizeData Параметры считываемых данных
+/// @param sizeLabel Параметры считываемых меток
+/// @param qty Количество, считываемое за раз
+/// @param step Шаг считывания
+/// @return Состояние считывания
 Function wineTrainData(filename : PAnsiChar;
                            data_ : PPSingle;
                            label_ : PPByte;
@@ -59,6 +116,16 @@ Function wineTrainData(filename : PAnsiChar;
                           qty: Integer; step: Integer
                            ) : keras.TStatus; cdecl; external KERAS_EXPORT;
 
+/// @brief titanicTrainData Считывание данных о выживаемости пассажиров на борту Титаника
+/// @param filename Имя файла с данными
+/// @param data Хранилище данных для записи
+/// @param label Хранилище меток для записи
+/// @param sizeData Параметры считываемых данных
+/// @param sizeLabel Параметры считываемых меток
+/// @param qty Количество, считываемое за раз
+/// @param step Шаг считывания
+/// @return Состояние считывания
+
 Function titanicTrainData(filename : PAnsiChar;
                            data_ : PPSingle;
                            label_ : PPByte;
@@ -66,9 +133,17 @@ Function titanicTrainData(filename : PAnsiChar;
                            sizeLabel : PLayerSize;
                           qty: Integer; step: Integer
                            ) : keras.TStatus; cdecl; external KERAS_EXPORT;
+
+/// @brief Освобождение памяти
 Procedure freeTrainData(data_ : PPSingle; label_ : PPByte); cdecl; external KERAS_EXPORT;
 
+/// @brief Освобождение памяти
 Procedure freeTrainDataF(data_ : PPSingle; label_ : PPSingle); cdecl; external KERAS_EXPORT;
+
+/// @brief Записывает последнюю ошибку в буфер
+/// @param buffer Буфер
+/// @param length Размер буфера
+Procedure dsLastError(buffer: PAnsiChar; length: Cardinal); cdecl; external KERAS_EXPORT;
 
 implementation
 
